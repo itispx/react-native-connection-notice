292c7e6f57af6437d2fe481055148d5f12b8867a
urboypx <guilhermeciotapx@outlook.com>
2022-10-17 02:26:22 -0300

    fix: working in ts

--- demo-ts/App.tsx
+++ demo-ts/App.tsx
@@ -3,7 +3,6 @@ import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import ConnectionIndicator from "react-native-connection-indicator";
-// import ConnectionIndicator from "../lib/src";
 
 const App: React.FC = () => {
   return (
