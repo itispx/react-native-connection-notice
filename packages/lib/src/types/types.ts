51a092eb918eb7425cfa81d3c73f2310a4ae74eb
urboypx <guilhermeciotapx@outlook.com>
2022-10-20 14:42:10 -0300

    feat: types

--- packages/lib/src/types/types.ts
+++ packages/lib/src/types/types.ts
@@ -3,12 +3,16 @@ import { StyleProp, ViewStyle, TextStyle } from "react-native";
 export interface IBaseProps {
   style?: StyleProp<ViewStyle>;
   styleText?: StyleProp<TextStyle>;
+  type: "internet" | "bluetooth";
   height?: number;
   offlineColor?: string;
   offlineText?: string;
   onlineColor?: string;
   onlineText?: string;
+  bluetoothColor?: string;
+  bluetoothText?: string;
   slideDuration?: number;
+  keepBluetooth?: number | boolean;
 }
 
 export interface IConnectionIndicatorProps extends IBaseProps {
