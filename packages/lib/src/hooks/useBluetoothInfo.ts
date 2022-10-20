ff10962d45bc42e89a9901b13511cb7ccdf5d0d4
urboypx <guilhermeciotapx@outlook.com>
2022-10-20 14:42:31 -0300

    feat: index logic

---
+++ packages/lib/src/hooks/useBluetoothInfo.ts
@@ -0,0 +1,5 @@
+const useBluetoothInfo = () => {
+  return false;
+};
+
+export default useBluetoothInfo;
--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -3,13 +3,20 @@ import React from "react";
 import ConnectionIndicator from "./components/ConnectionIndicator";
 
 import useNetworkInfo from "./hooks/useNetworkInfo";
+import useBluetoothInfo from "./hooks/useBluetoothInfo";
 
 import { IBaseProps } from "./types/types";
 
 const withComponent: React.FC<IBaseProps> = (props) => {
   const isInternetConnected = useNetworkInfo();
+  const isBluetoothConnected = useBluetoothInfo();
 
-  return <ConnectionIndicator isConnected={isInternetConnected} {...props} />;
+  return (
+    <ConnectionIndicator
+      isConnected={props.type === "internet" ? isInternetConnected : isBluetoothConnected}
+      {...props}
+    />
+  );
 };
 
 export default withComponent;
