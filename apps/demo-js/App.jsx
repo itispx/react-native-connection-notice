d115375066288a50b3943d2f62686ca066d12156
urboypx <guilhermeciotapx@outlook.com>
2022-10-21 03:14:02 -0300

    feat: demos

--- apps/demo-js/App.jsx
+++ apps/demo-js/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import ConnectionNotice, { useNetworkInfo } from "react-native-connection-notice";
 
 const App = () => {
+  const isConnected = useNetworkInfo();
+
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
       <StatusBar style="auto" />
+      {/* your components */}
       <ConnectionNotice />
+      <Text style={{ marginTop: 90 }}>{isConnected ? "Connected" : "Not connected"}</Text>
     </View>
   );
 };
--- apps/demo-ts/App.tsx
+++ apps/demo-ts/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import ConnectionNotice, { useNetworkInfo } from "react-native-connection-notice";
 
 const App: React.FC = () => {
+  const isConnected = useNetworkInfo();
+
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
       <StatusBar style="auto" />
+      {/* your components */}
       <ConnectionNotice />
+      <Text style={{ marginTop: 90 }}>{isConnected ? "Connected" : "Not connected"}</Text>
     </View>
   );
 };
